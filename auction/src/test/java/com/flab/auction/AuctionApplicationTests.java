b657022c4f25f1028307e3777fcff1156cb5db52
sjaqjwor <lsklsk45@naver.com>
2023-08-30 14:35:51 +0900

    feat: 서비스 분리 및 프로젝트 init 작업

---
+++ auction/src/test/java/com/flab/auction/AuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.auction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
